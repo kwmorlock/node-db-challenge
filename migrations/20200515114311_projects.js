6118a59cff95adf78ea0125647f02b2bd6fe1d49
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-15 11:51:29 -0400

    projects table created

--- migrations/20200515114311_projects.js
+++ migrations/20200515114311_projects.js
@@ -1,6 +1,15 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments();
+        tbl.string('project_name', 128)
+        .unique()
+        .notNullable();
+        tbl.string('description', 128);
+        tbl.boolean('completed')
+        .defaultTo(false);
+    })
 };
 
 exports.down = function(knex) {
