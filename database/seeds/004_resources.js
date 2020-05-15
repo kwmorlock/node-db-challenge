709f802d4e8451b30824cd9d96bf73735e3e4900
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-15 13:12:13 -0400

    resources seed setup

--- database/seeds/004_resources.js
+++ database/seeds/004_resources.js
@@ -1,13 +1,7 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('resources').insert([
+    {resources_name: 'ummm'},
+    {resources_name: 'ok'},
+    {resources_name: 'Alright'},
+  ]);
 };
