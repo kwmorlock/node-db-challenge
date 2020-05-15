874abdf96fcf39cdc79045b4568479f4ca40d5ce
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-15 13:50:36 -0400

    gets for tasks, resources, project-resources

--- router/project-router.js
+++ router/project-router.js
@@ -4,7 +4,7 @@ const db = require("../db-config");
 
 const router = express.Router();
 
-router.get("/projects", (req, res) => {
+router.get("/", (req, res) => {
     db("projects") 
       .then(project => {
         res.json(project);
@@ -13,3 +13,35 @@ router.get("/projects", (req, res) => {
         res.status(500).json({ message: "Failed to retrieve project" });
       });
   });
+
+  router.get("/tasks", (req, res) => {
+    db("tasks") 
+      .then(task => {
+        res.json(task);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Failed to retrieve project" });
+      });
+  });
+
+  router.get("/resources", (req, res) => {
+    db("resources") 
+      .then(resource => {
+        res.json(resource);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Failed to retrieve project" });
+      });
+  });
+
+  router.get("/projects_resources", (req, res) => {
+    db("projects_resources") 
+      .then(resource => {
+        res.json(resource);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Failed to retrieve project" });
+      });
+  });
+
+  module.exports = router;
