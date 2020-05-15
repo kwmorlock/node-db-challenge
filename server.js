d5d53dfc5a0d04c2034aa4b81018f687b0f7597c
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-15 13:38:31 -0400

    server updated

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
-const ProjectRouter = require('./projects/project-router.js');
-
 const server = express();
 
+const projectRouter = require('./router/project-router.js');
+
+const helmet = require("helmet");
+
+
+server.use(helmet());
+
+
+
 server.use(express.json());
-server.use('/api/projects', ProjectRouter);
+server.use('/api/projects', projectRouter);
 //line 9 is starting point for site to use in postman
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some functioning stuff for this sprint!</h2>`);
+  });
 
 module.exports = server;
