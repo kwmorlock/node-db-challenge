8342df166e64fdcfaf91572319136f478bbfb389
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-15 11:36:36 -0400

    useNullAsDefault, seeds, and pool added to knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,20 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/projects.db3'
     }
   },
+  useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations' // put migrations here
+    },
+    seeds: {
+      directory: './database/seeds' // put seeds here
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
 
 };
